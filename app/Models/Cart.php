07b994705da8ede10b4c407bb9ea90795cd48096
Renandatta <renandattarooziq@gmail.com>
2021-04-07 00:18:16 +0700

    Cart

---
+++ app/Models/Cart.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property mixed produk
+ * @property mixed qty
+ */
+class Cart extends Model
+{
+    protected $table = 'cart';
+    protected $fillable = [
+        'user_id',
+        'produk_id',
+        'qty'
+    ];
+    protected $with = ['produk'];
+
+    public function produk()
+    {
+        return $this->belongsTo(Produk::class, 'produk_id', 'id');
+    }
+
+    public function getTotalAttribute()
+    {
+        return $this->produk->harga * $this->qty;
+    }
+}
---
+++ database/migrations/2021_04_05_230206_create_carts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCartsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cart', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('produk_id');
+            $table->integer('qty')->default(0);
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('produk_id')->references('id')->on('produk');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cart');
+    }
+}
